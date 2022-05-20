08941245f1486ec24e68a25127cad37bdb237fea
Miracle <umuraliev.r@gmail.com>
2022-05-20 07:18:52 +0000

    change

--- app/settings.py
+++ app/settings.py
@@ -160,8 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_HOST_USER = 'umuraliev.photo@gmail.com'
+EMAIL_HOST_PASSWORD = '123456rm'
 
 
 
